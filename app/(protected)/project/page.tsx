e923c8379a620f84d0810a1f1c8980693933707e
Wheza <wheza99@gmail.com>
2025-05-18 11:17:34 +0700

    implement theme switcher
    implement theme switcher

--- app/(protected)/project/page.tsx
+++ app/(protected)/project/page.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ProjectTheme1 from './theme-1'
+"use client";
+
+import ProjectTheme1 from './theme-1';
+import { TemplateSwitcher } from "../../../components/layout/template-switcher";
+import { useState } from "react";
 
 export default function ProjectPage() {
+  const [activeTheme, setActiveTheme] = useState<"theme-1" | "theme-2" | "theme-3" | "theme-4" | "theme-5" | "theme-6" | "theme-7" | "theme-8" | "theme-9" | "theme-10">("theme-1");
+  
+  // Handle theme change
+  const handleThemeChange = (theme: string) => {
+    setActiveTheme(theme as "theme-1" | "theme-2" | "theme-3" | "theme-4" | "theme-5" | "theme-6" | "theme-7" | "theme-8" | "theme-9" | "theme-10");
+  };
+
+  // Render the appropriate theme component based on activeTheme
+  const renderTheme = () => {
+    switch (activeTheme) {
+      case "theme-1":
+        return <ProjectTheme1 />;
+      // Add other themes when they become available
+      // case "theme-2":
+      //   return <ProjectTheme2 />;
+      // case "theme-3":
+      //   return <ProjectTheme3 />;
+      // ...
+      default:
+        return <ProjectTheme1 />;
+    }
+  };
+
   return (
-    <div>
-      <ProjectTheme1 />
-    </div>
-  )
+    <>
+      <TemplateSwitcher onThemeChange={handleThemeChange} activeTheme={activeTheme} />
+      {renderTheme()}
+    </>
+  );
 }
